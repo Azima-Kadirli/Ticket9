77c419aaeca95b2e4023834e9bee084c47ce8c17
Chinara Karimzada <chinarankh-mpa101@code.edu.az>
2026-01-19 11:46:34 +0400

    Add project files.

---
+++ Ticket9/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using Ticket9.Models.Common;
+
+namespace Ticket9.Models
+{
+    public class Category:BaseEntity
+    {
+        public string Name { get; set; }
+        public ICollection<Employee> Employees { get; set; }
+    }
+}
---
+++ Ticket9/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿using Ticket9.Models.Common;
+
+namespace Ticket9.Models
+{
+    public class Employee:BaseEntity
+    {
+        public string FullName { get; set; }
+        public string Image { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
